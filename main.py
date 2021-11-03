1edfae52cc8a47eec3c3a693fc793157265798ee
Tom Y Liu <tliu46@node1163.oscar.ccv.brown.edu>
2021-11-03 00:04:36 -0400

    .

--- main.py
+++ main.py
@@ -16,11 +16,6 @@ import yaml
 
 import shutil
 
-# ignore warnings if on gpu
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-import tensorflow as tf
-
 def main():
     tf.compat.v1.disable_eager_execution()
     config = configure()
