59d494ee90ca1da85e26b91b617538e0c230f909
Tom Y Liu <tliu46@node1101.oscar.ccv.brown.edu>
2021-11-02 23:56:02 -0400

    remove gpu

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#SBATCH -p gpu --gres=gpu:2
 #SBATCH -n 1
 #SBATCH --mem=64G
-#SBATCH -t 3:00:00
+#SBATCH -t 48:00:00
 
-module load cuda
 module load anaconda/2020.02
 source /gpfs/runtime/opt/anaconda/2020.02/etc/profile.d/conda.sh
 conda activate csci2951x
