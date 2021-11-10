40e11bf7a189669f6911bc1c47a2c2f466abe11c
projectxanon <projectxanonymous2021@gmail.com>
2021-11-10 13:42:21 -0500

    debug

--- trainers/curriculum.py
+++ trainers/curriculum.py
@@ -33,6 +33,8 @@ class CurriculumTrainer(object):
         self.tasks = []
         for hint_key, hint in self.hints.items():
             goal = util.parse_fexp(hint_key)
+            print("GOAL")
+            print(goal)
             goal = (self.subtask_index.index(goal[0]), self.cookbook.index[goal[1]])
             if config.model.use_args:
                 steps = [util.parse_fexp(s) for s in hint]
