d36b05d34e9b6bff5243516c31ecfcf4da88d4df
projectxanon <projectxanonymous2021@gmail.com>
2021-12-12 13:56:57 -0500

    added % of eg reward to plots

--- plots_drawer.py
+++ plots_drawer.py
@@ -6,14 +6,20 @@ import matplotlib.cm as cm
 from numpy.random import random
 
 data = defaultdict(list)
+original_numbers = []
 with open('rewards.csv', newline='') as csvfile:
      spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
      i = 0
      for row in spamreader:
 
-         if i == 0:
+         if i == 0 or i == 2:
              i += 1
              continue
+
+         if i == 1:
+            original_numbers = row
+            i += 1
+            continue
 
          experiment = row[0]
          type = row[1]
@@ -24,13 +30,15 @@ with open('rewards.csv', newline='') as csvfile:
          r5 = row[6]
          data[experiment].append([type,r1,r2,r3,r4,r5])
 
+counter = 0
 for experiment in data.keys():
     x = []
     y = []
+    counter += 1
     for idx, type in enumerate(data[experiment]):
         row = type
         x += [1,2,3,4,5]
-        y += [float(row[1]), float(row[2]), float(row[3]), float(row[4]), float(row[5])]
+        y += [float(row[1])/float(original_numbers[2]), float(row[2])/float(original_numbers[3]), float(row[3])/float(original_numbers[4]), float(row[4])/float(original_numbers[5]), float(row[5])/float(original_numbers[6])]
 
     colors = cm.rainbow(np.linspace(0, 1, len(y)))
     i = 0
@@ -38,15 +46,9 @@ for experiment in data.keys():
 
     for curr_y, c in zip(y, colors):
         f = plt.scatter(x[i], curr_y, color=c, label = curr_y)
-        # plt.legend((f),
-        #        ("Type 1: Purple", "Type 2: Blue", "Type 3: Turquoise", "Type 4: Green", "Type 5: Orange"),
-        #        scatterpoints=1,
-        #        loc='lower left',
-        #        ncol=3,
-        #        fontsize=8)
         i += 1
-    # colors = ['p', 'b', 't', 'g', 'o']
-    plt.title("Experiment: " + str(experiment))
+
+    plt.title("Experiment " + str(counter) + ": " + str(experiment))
     classes = ["1", "2", "3", "4", "5"]
     class_colours = ['#8A2BE2','#6495ED','#00FFFF', '#98FB98', '#FFA500']
     recs = []
@@ -54,7 +56,6 @@ for experiment in data.keys():
 
     for i in range(0,len(class_colours)):
         recs.append(mpatches.Rectangle((0,0),1,1,fc=class_colours[i]))
+
     plt.legend(recs,classes,loc=4)
-
-
     plt.show()
