1eef2d3a9d0421360a39c7a3fb3d7e7c4f0dcf3d
projectxanon <projectxanonymous2021@gmail.com>
2021-12-12 14:06:39 -0500

    legend outside plot

--- plots_drawer.py
+++ plots_drawer.py
@@ -35,27 +35,55 @@ for experiment in data.keys():
     x = []
     y = []
     counter += 1
-    for idx, type in enumerate(data[experiment]):
-        row = type
-        x += [1,2,3,4,5]
-        y += [float(row[1])/float(original_numbers[2]), float(row[2])/float(original_numbers[3]), float(row[3])/float(original_numbers[4]), float(row[4])/float(original_numbers[5]), float(row[5])/float(original_numbers[6])]
+    if counter == 1:
+        for idx, type in enumerate(data[experiment]):
+            row = type
+            x += [1,2,3,4,5]
+            y += [float(row[1])/float(original_numbers[2]), float(row[2])/float(original_numbers[3]), float(row[3])/float(original_numbers[4]), float(row[4])/float(original_numbers[5]), float(row[5])/float(original_numbers[6])]
 
-    colors = cm.rainbow(np.linspace(0, 1, len(y)))
-    i = 0
-    legend_str = []
+        colors = cm.rainbow(np.linspace(0, 1, len(y)))
+        i = 0
+        legend_str = []
 
-    for curr_y, c in zip(y, colors):
-        f = plt.scatter(x[i], curr_y, color=c, label = curr_y)
-        i += 1
+        for curr_y, c in zip(y, colors):
+            f = plt.scatter(x[i], curr_y, color=c, label = curr_y)
+            i += 1
 
-    plt.title("Experiment " + str(counter) + ": " + str(experiment))
-    classes = ["1", "2", "3", "4", "5"]
-    class_colours = ['#8A2BE2','#6495ED','#00FFFF', '#98FB98', '#FFA500']
-    recs = []
-    import matplotlib.patches as mpatches
+        plt.title("Experiment " + str(counter) + ": " + str(experiment))
+        classes = ["1", "2", "3", "4", "5"]
+        class_colours = ['#8A2BE2','#6495ED','#00FFFF', '#98FB98', '#FFA500']
+        recs = []
+        import matplotlib.patches as mpatches
 
-    for i in range(0,len(class_colours)):
-        recs.append(mpatches.Rectangle((0,0),1,1,fc=class_colours[i]))
+        for i in range(0,len(class_colours)):
+            recs.append(mpatches.Rectangle((0,0),1,1,fc=class_colours[i]))
 
-    plt.legend(recs,classes,loc=4)
-    plt.show()
+        plt.legend(recs,classes,bbox_to_anchor=(1.1, 1.05))
+        plt.show()
+    else:
+        for idx, type in enumerate(data[experiment]):
+            row = type
+            x += [1,2,3,4,5]
+            y += [float(row[1])/float(original_numbers[2]), float(row[2])/float(original_numbers[3]), float(row[3])/float(original_numbers[4]), float(row[4])/float(original_numbers[5]), float(row[5])/float(original_numbers[6])]
+
+        colors = cm.rainbow(np.linspace(0, 1, len(y)))
+        i = 0
+        legend_str = []
+        if 0 in y:
+            print("TRUE")
+
+        for curr_y, c in zip(y, colors):
+            f = plt.scatter(x[i], curr_y, color=c, label = curr_y)
+            i += 1
+
+        plt.title("Experiment " + str(counter) + ": " + str(experiment))
+        classes = ["1", "2", "3", "4", "5", "6"]
+        class_colours = ['#8A2BE2','#6495ED','#00FFFF', '#98FB98', '#FFA500', 'red']
+        recs = []
+        import matplotlib.patches as mpatches
+
+        for i in range(0,len(class_colours)):
+            recs.append(mpatches.Rectangle((0,0),1,1,fc=class_colours[i]))
+
+        plt.legend(recs,classes,bbox_to_anchor=(1.1, 1.05))
+        plt.show()
